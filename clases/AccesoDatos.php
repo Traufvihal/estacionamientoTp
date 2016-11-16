cf8b05c8b2983ca2ddf42b563e15b80b629f3f7d
Traufvihal <santiago.sansone@gmail.com>
2016-11-16 20:05:41 -0300

    Pequeña correción

--- clases/AccesoDatos.php
+++ clases/AccesoDatos.php
@@ -8,7 +8,7 @@ class AccesoDatos
     {
         try {
             //$this->objetoPDO = new PDO('mysql:host=localhost;dbname=estacdb;charset=utf8', 'root', '', array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-            $this->objetoPDO = new PDO('mysql:host=mysql.hostinger.com.ar;dbname=u349695711_estdb;charset=utf8', ' 	u349695711_ssdb', '49175424estacdb', array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            $this->objetoPDO = new PDO('mysql:host=mysql.hostinger.com.ar;dbname=u349695711_estdb;charset=utf8', 'u349695711_ssdb', '49175424estacdb', array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
             $this->objetoPDO->exec("SET CHARACTER SET utf8");
             }
         catch (PDOException $e) {
